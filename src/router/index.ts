dc1db06fb4ade6b3f3dcc8ccbfdbd68ef7a61eea
PramAhmad <pramuditaadit2810@gmail.com>
2024-10-14 21:07:21 +0700

    update web history

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { allRoutes } from '@/router/router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: allRoutes
 });
 
